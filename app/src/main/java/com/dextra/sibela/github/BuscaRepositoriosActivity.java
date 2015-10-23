fd403d698399024ddb219ec1c6170074d5a09c94
Leonardo Sibela <leonardo.sibela@dls126.lab.dextra.com.br>
2015-10-23 16:55:39 -0200

    Removendo imports nao utilizados

--- app/src/main/java/com/dextra/sibela/github/BuscaRepositoriosActivity.java
+++ app/src/main/java/com/dextra/sibela/github/BuscaRepositoriosActivity.java
@@ -1,33 +1,15 @@
 package com.dextra.sibela.github;
 
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.dextra.sibela.github.bean.GithubUser;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class BuscaRepositoriosActivity extends AppCompatActivity {
 
-    Button btnPesquisar;
-    EditText txtRepoName;
+    private Button btnPesquisar;
+    private EditText txtRepoName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
