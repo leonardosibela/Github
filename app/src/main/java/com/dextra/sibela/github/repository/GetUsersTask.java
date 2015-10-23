ddb57a528a450da9e6f16527673f8133def38485
Leonardo Sibela <leonardo.sibela@dls126.lab.dextra.com.br>
2015-10-23 10:02:15 -0200

    Adicionado camada de repository para busca de usuarios

---
+++ app/src/main/java/com/dextra/sibela/github/repository/GetUsersTask.java
@@ -0,0 +1,15 @@
+package com.dextra.sibela.github.repository;
+
+import android.os.AsyncTask;
+
+public class GetUsersTask extends AsyncTask {
+
+
+    @Override
+    protected Object doInBackground(Object... params) {
+
+        String login = (String) params[0];
+
+        return null;
+    }
+}
