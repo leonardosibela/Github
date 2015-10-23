679e391c0f87342c22e9cdddaafb5568fa51c2d3
Leonardo Sibela <leonardo.sibela@dls126.lab.dextra.com.br>
2015-10-23 15:51:01 -0200

    Fix

--- app/src/main/java/com/dextra/sibela/github/BuscaRepositoriosActivity.java
+++ app/src/main/java/com/dextra/sibela/github/BuscaRepositoriosActivity.java
@@ -7,6 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.dextra.sibela.github.bean.GithubUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +20,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class BuscaRepositoriosActivity extends AppCompatActivity {
@@ -35,69 +43,10 @@ public class BuscaRepositoriosActivity extends AppCompatActivity {
 
                 String repoName = txtRepoName.getText().toString();
 
-                new GetUsersTask().execute(repoName);
+                //TODO: Buscar repositorios
 
             }
         });
 
-    }
-
-    public class GetUsersTask extends AsyncTask<String, Integer, String> {
-
-        private String readStream(InputStream in) {
-            BufferedReader reader = null;
-            StringBuffer response = new StringBuffer();
-            try {
-                reader = new BufferedReader(new InputStreamReader(in));
-                String line = "";
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            return response.toString();
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            String response = "";
-
-            String urlPrefix = "https://api.github.com/search/users?q=";
-            String login = (String) params[0];
-            String urlSulfix = "%20type:users";
-
-            String strUrl = urlPrefix + login + urlSulfix;
-
-            try {
-                URL url = new URL(strUrl);
-
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-                response = readStream(urlConnection.getInputStream());
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return response;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-        }
     }
 }
--- app/src/main/java/com/dextra/sibela/github/BuscaUsuariosActivity.java
+++ app/src/main/java/com/dextra/sibela/github/BuscaUsuariosActivity.java
@@ -1,10 +1,27 @@
 package com.dextra.sibela.github;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.dextra.sibela.github.bean.GithubUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BuscaUsuariosActivity extends AppCompatActivity {
 
@@ -25,10 +42,94 @@ public class BuscaUsuariosActivity extends AppCompatActivity {
 
                 String nomeUsuario = txtUsername.getText().toString();
 
-                // TODO: Pesquisar usuarios
+                new GetUsersTask().execute(nomeUsuario);
 
             }
         });
+
+    }
+
+    public class GetUsersTask extends AsyncTask<String, Integer, String> {
+
+        private String readStream(InputStream in) {
+            BufferedReader reader = null;
+            StringBuffer response = new StringBuffer();
+            try {
+                reader = new BufferedReader(new InputStreamReader(in));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            return response.toString();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String response = "";
+
+            String urlPrefix = "https://api.github.com/search/users?q=";
+            String login = (String) params[0];
+            String urlSulfix = "%20type:users";
+
+            String strUrl = urlPrefix + login + urlSulfix;
+
+            try {
+                URL url = new URL(strUrl);
+
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                response = readStream(urlConnection.getInputStream());
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+        }
+
+        private List<GithubUser> jsonToList(String strJsonArray) {
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(strJsonArray);
+                List<GithubUser> githubUsers = new ArrayList<>();
+
+                for(int i = 0; i < jsonArray.length(); i++) {
+
+                    String strGithubUser = jsonArray.get(i).toString();
+                    JSONObject githubUser = new JSONObject(strGithubUser);
+
+                    // githubUsers.add(githubUser);
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
 
     }
 }
