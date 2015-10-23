b0ea699f196891657df0fc2869f4be538cb33fc3
Leonardo Sibela <leonardo.sibela@dls126.lab.dextra.com.br>
2015-10-23 15:36:23 -0200

    Alteracao AsyncTask - Inner class da activity

--- app/src/main/java/com/dextra/sibela/github/BuscaRepositoriosActivity.java
+++ app/src/main/java/com/dextra/sibela/github/BuscaRepositoriosActivity.java
@@ -1,10 +1,20 @@
 package com.dextra.sibela.github;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class BuscaRepositoriosActivity extends AppCompatActivity {
 
@@ -25,10 +35,69 @@ public class BuscaRepositoriosActivity extends AppCompatActivity {
 
                 String repoName = txtRepoName.getText().toString();
 
-                // TODO: Pesquisar repositorios
+                new GetUsersTask().execute(repoName);
 
             }
         });
 
+    }
+
+    public class GetUsersTask extends AsyncTask<String, Integer, String> {
+
+        private String readStream(InputStream in) {
+            BufferedReader reader = null;
+            StringBuffer response = new StringBuffer();
+            try {
+                reader = new BufferedReader(new InputStreamReader(in));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            return response.toString();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String response = "";
+
+            String urlPrefix = "https://api.github.com/search/users?q=";
+            String login = (String) params[0];
+            String urlSulfix = "%20type:users";
+
+            String strUrl = urlPrefix + login + urlSulfix;
+
+            try {
+                URL url = new URL(strUrl);
+
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                response = readStream(urlConnection.getInputStream());
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+        }
     }
 }
