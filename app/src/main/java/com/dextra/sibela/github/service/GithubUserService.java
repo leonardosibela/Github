44455af7267266e0ffefb4cc2bd1c63a5aeead8e
Leonardo Sibela <leonardo.sibela@dls126.lab.dextra.com.br>
2015-10-23 10:01:11 -0200

    Adicionado camada de servico para busca de usuarios

---
+++ app/src/main/java/com/dextra/sibela/github/service/GithubUserService.java
@@ -0,0 +1,19 @@
+package com.dextra.sibela.github.service;
+
+import com.dextra.sibela.github.bean.GithubUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GithubUserService {
+
+    public List<GithubUser> searchUsers(String login) {
+
+        List<GithubUser> users = new ArrayList<>();
+
+        String strUsers;
+
+        return users;
+    }
+
+}
